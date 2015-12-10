0b1ff7cc16ff8d877bbb8d2dd7482d8403f7fdcb
timvisee <timvisee@gmail.com>
2015-12-10 15:37:47 +0100

    Created Led class

---
+++ Led.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tim on 10-12-2015.
+//
+
+#include "Led.h"
---
+++ Led.h
@@ -0,0 +1,14 @@
+//
+// Created by Tim on 10-12-2015.
+//
+
+#ifndef NUMBERGUESSGAME_LED_H
+#define NUMBERGUESSGAME_LED_H
+
+
+class Led {
+
+};
+
+
+#endif //NUMBERGUESSGAME_LED_H
