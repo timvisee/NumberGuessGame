826e7b8fc9176b32502a391111068c5d4ae7967d
timvisee <timvisee@gmail.com>
2015-12-10 19:19:34 +0100

    Added docs for Timer class

--- Timer.h
+++ Timer.h
@@ -3,28 +3,70 @@
 
 #include <Arduino.h>
 
+/**
+ * Timer class.
+ */
 class Timer {
 private:
+    /**
+     * Timeout delay in milliseconds.
+     * */
     int timeoutDelay;
+
+    /**
+     * Timeout as time in milliseconds, represents millis() value.
+     * */
     long timeout;
 
 public:
+    /**
+     * Constructor.
+     */
     Timer();
 
+    /**
+     * Constructor.
+     *
+     * @param timeout Timeout in milliseconds.
+     */
     Timer(int timeout);
 
+    /**
+     * Get the timeout delay in milliseconds.
+     *
+     * @return Timeout delay.
+     */
     int getTimeoutDelay();
 
+    /**
+     * Set the timeout delay in milliseconds.
+     *
+     * @return Timeout delay.
+     */
     void setTimeoutDelay(int timeout);
 
+    /**
+     * Start the timer.
+     */
     void start();
 
+    /**
+     * Start the timer with the specified delay in milliseconds.
+     * This also updates the default delay of this timer.
+     */
     void start(int timeoutDelay);
 
+    /**
+     * Check whether the timer is finished.
+     *
+     * @return True if finished, false if not.
+     */
     bool isFinished();
 
+    /**
+     * Stop the timer if it's currently active.
+     */
     void stop();
 };
-
 
 #endif //NUMBERGUESSGAME_TIMER_H
