278c92859507fc0bd7aea7d45f7851a85d90ed52
timvisee <timvisee@gmail.com>
2015-12-10 13:23:26 +0100

    Created Timer class header

--- Timer.h
+++ Timer.h
@@ -1,13 +1,23 @@
-//
-// Created by Tim on 10-12-2015.
-//
-
 #ifndef NUMBERGUESSGAME_TIMER_H
 #define NUMBERGUESSGAME_TIMER_H
 
 
 class Timer {
+private:
+    int timeout;
 
+public:
+    Timer(int timeout);
+
+    int getTimeout();
+
+    int setTimeout(int timeout);
+
+    void start();
+
+    bool isFinished();
+
+    void stop();
 };
 
 
