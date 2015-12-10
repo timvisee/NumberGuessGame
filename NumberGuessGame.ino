6b533f367bd7ee61c2c3424be9a774d4f539149e
timvisee <timvisee@gmail.com>
2015-12-10 14:18:13 +0100

    Added button handling, showing answer

--- NumberGuessGame.ino
+++ NumberGuessGame.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "Button.h"
+#include "Timer.h"
 
 /** LED count. */
 const int LED_COUNT = 4;
@@ -10,8 +11,14 @@ const int BUTTON_PIN = 2;
 
 const int LED_DURATION = 1500;
 
+const int TIMER_WAIT_DELAY = 2000;
+const int TIMER_SHOW_ANSWER_DURATION = 500;
+
 /** Button instance. */
 Button btn(BUTTON_PIN);
+
+/** Timer instance. */
+Timer timer(TIMER_WAIT_DELAY);
 
 /**
  * Called once on set up.
@@ -40,25 +47,48 @@ void loop() {
     // Show the actual number, and wait for a little while
     showNumber(num);
     delay(LED_DURATION);
+    showNumber(0);
 
-    // Update the button state
-    btn.update();
+    // Start the timer
+    timer.start();
 
+    // Define a variable to store the number in the user has entered
+    int answer = 0;
 
+    // Use a while loop to handle the button presses
+    while(!timer.isFinished()) {
+        // Update the button state
+        btn.update();
 
-    // Increase the counter if the button is pressed
-    if(btn.isPressedOnce())
-        counter++;
+        // Handle button presses
+        if(btn.isPressedOnce()) {
+            // Increase the answer
+            answer++;
+
+            // Reset the answer
+            timer.start();
+        }
+    }
+
+    // Stop the timer
+    timer.stop();
+
+    // Show the result the user has entered
+    showNumber(answer);
+    delay(TIMER_SHOW_ANSWER_DURATION);
+    showNumber(0);
 }
 
 /**
  * Show a number, using the four LEDs.
+ *
+ * @param number The number to show the LEDs for.
  */
-void showNumber(int n) {
+void showNumber(int number) {
     for(byte i = 0; i < LED_COUNT; i++) {
         // Turn the LED on or off, based on the number
-        digitalWrite(LED_PINS[i], n & 1);
-        n /= 2;
+        digitalWrite(LED_PINS[i], number & 1);
+        number /= 2;
     }
 }
 
