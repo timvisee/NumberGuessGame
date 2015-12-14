a9f0934d18b88a6e52e1be5cd02d762bcd7561ae
Tim Visée <timvisee@gmail.com>
2015-12-14 17:11:21 +0100

    Created Core class, for all basic logic

---
+++ Core.cpp
@@ -0,0 +1,17 @@
+/**
+ * NumberGuessGame.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#include "Core.h"
+
+void Core::setup() {
+
+}
+
+void Core::loop() {
+
+}
---
+++ Core.h
@@ -0,0 +1,28 @@
+/**
+ * NumberGuessGame.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#ifndef NUMBERGUESSGAME_CORE_H
+#define NUMBERGUESSGAME_CORE_H
+
+/**
+ * Core class.
+ */
+class Core {
+public:
+    /**
+     * Called once on set up.
+     */
+    void setup();
+
+    /**
+     * Called each loop.
+     */
+    void loop();
+};
+
+#endif //NUMBERGUESSGAME_CORE_H
