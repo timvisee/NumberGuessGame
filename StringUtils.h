85d6540577485cb2ce8c6da7dce04f5e2f3a95fd
Tim Visée <timvisee@gmail.com>
2015-12-14 16:47:06 +0100

    Added docs to StringUtils class

--- StringUtils.h
+++ StringUtils.h
@@ -13,9 +13,31 @@
 #include "StandardCplusplus.h"
 #include <vector>
 
+/**
+ * StringUtils class.
+ * A class for some generic string utilities.
+ */
 class StringUtils {
 public:
+    // TODO: Remove the 'smart' parameter, because it isn't used?
+	/**
+	 * Split a string by the specified split character.
+	 *
+	 * @param s String to split.
+	 * @param splitChar The character to split the string at.
+	 * @param smart
+	 *
+	 * @return A vector of string parts.
+	 */
     static std::vector<String> split(String s, char splitChar, bool smart);
+
+	/**
+	 * Get the number of specific characters in a string.
+	 *
+	 * @param c Type of character to count.
+	 *
+	 * @return The number of characters, in the string.
+	 */
 	static int getCharacterCount(String s, char c);
 };
 
