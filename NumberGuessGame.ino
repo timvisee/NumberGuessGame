2a10bb40927cc760382f0d2357e0b775fe41a021
Tim Visée <timvisee@gmail.com>
2015-12-14 17:07:39 +0100

    Moved configuration constants to Global.h file

--- NumberGuessGame.ino
+++ NumberGuessGame.ino
@@ -17,89 +17,10 @@
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "Globals.h"
 #include "Button.h"
 #include "Timer.h"
 #include "Led.h"
-
-/**
- * Screen LED count.
- */
-const int SCREEN_LED_COUNT = 4;
-
-/**
- * Screen LED pins.
- */
-const int SCREEN_LED_PINS[] = {3, 5, 6, 9};
-
-/**
- * Green LED pin.
- */
-const int GREEN_LED_PIN = 10;
-
-/**
- * Red LED pin.
- */
-const int RED_LED_PIN = 11;
-
-/**
- * Button pin.
- */
-const int BUTTON_PIN = 2;
-
-/**
- * The delay in milliseconds for the game to start.
- */
-const int START_DELAY = 2000;
-
-/**
- * Pulse duration in milliseconds.
- */
-const int PULSE_DURATION = 1000;
-
-/**
- * Pulse brightness lowest.
- */
-const int PULSE_BRIGHTNESS_LOW = 8;
-
-/**
- * Pulse brightness highest.
- */
-const int PULSE_BRIGHTNESS_HIGH = 80;
-
-/**
- * The time in milliseconds to show the answer.
- */
-const int ANSWER_VISIBLE_DURATION = 1500;
-
-/**
- * The time in milliseconds for the input timeout.
- */
-const int INPUT_TIMEOUT = 2000;
-
-/**
- * The feedback low brightness value.
- */
-const int FEEDBACK_BRIGHTNESS_LOW = 0;
-
-/**
- * The feedback high brightness value.
- */
-const int FEEDBACK_BRIGHTNESS_HIGH = 80;
-
-/**
- * The feedback visible duration in milliseconds.
- */
-const int FEEDBACK_VISIBLE_DURATION = 100;
-
-/**
- * The number of milliseconds the user's input is visible.
- */
-const int USER_INPUT_VISIBLE_DURATION = 1000;
-
-/**
- * The serial baud rate.
- */
-const int SERIAL_BAUD = 9600;
 
 /**
  * Screen LED instances.
