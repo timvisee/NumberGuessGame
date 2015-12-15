fdf48d245a8f98cb4f51b873ffae4748e3bf0558
Tim Visée <timvisee@gmail.com>
2015-12-15 21:13:50 +0100

    Added docs to Packet class

--- Packet.h
+++ Packet.h
@@ -54,29 +54,161 @@ private:
     String * strArr;
 
 public:
+    /**
+     * Constructor.
+     */
 	Packet();
+
+    /**
+     * Constructor.
+     *
+     * @param targetDeviceId ID of the target device.
+     * @param packetType The type of the packet.
+     */
 	Packet(byte targetDeviceId, byte packetType);
+
+    /**
+     * Constructor.
+     *
+     * @param targetDeviceId ID of the target device.
+     * @param packetType The type of the packet.
+     * @param intArr The integer array.
+     * @param intSize The size of the integer array.
+     * @param boolArr The boolean array.
+     * @param boolSize The size of the boolean array.
+     * @param strArr The string array.
+     * @param strSize The size of the string array.
+     */
 	Packet(byte targetDeviceId, byte packetType,
            uint8_t intSize, int * intArr,
            uint8_t boolSize, bool * boolArr,
            uint8_t strSize, String * strArr);
+
+    /**
+     * Destructor.
+     */
 	~Packet();
+
+    /**
+     * Get the ID of the target device.
+     *
+     * @return Target device ID.
+     */
     byte getTargetDeviceId();
-	void setTargetDeviceId(byte uint8_t);
+
+    /**
+     * Set the ID of the target device.
+     *
+     * @param id Target device ID.
+     */
+	void setTargetDeviceId(byte id);
+
+    /**
+     * Get the packet type ID.
+     *
+     * @return Packet type ID.
+     */
     byte getPacketType();
-	void setPacketType(byte uint8_t);
+
+    /**
+     * Set the packet type ID.
+     *
+     * @param type Packet type ID.
+     */
+	void setPacketType(byte type);
+
+    /**
+     * Get the integers array.
+     *
+     * @return Integers.
+     */
 	int * getIntegers();
+
+    /**
+     * Get the number of integers in the integer array.
+     *
+     * @return Integer array size.
+     */
 	uint8_t getIntegersCount();
+
+    /**
+     * Check whether this packet has any integers.
+     *
+     * @return True if there are any integers, false otherwise.
+     */
 	bool hasIntegers();
+
+    /**
+     * Set the integers array.
+     *
+     * @param intArr Integer array.
+     * @param size Integer array size.
+     */
 	void setIntegers(int * intArr, uint8_t size);
+    
+    /**
+     * Get the booleans array.
+     *
+     * @return Booleans.
+     */
 	bool * getBooleans();
+
+    /**
+     * Get the number of booleans in the boolean array.
+     *
+     * @return Boolean array size.
+     */
     uint8_t getBooleansCount();
+
+    /**
+     * Check whether this packet has any booleans.
+     *
+     * @return True if there are any booleans, false otherwise.
+     */
 	bool hasBooleans();
+
+    /**
+     * Set the booleans array.
+     *
+     * @param intArr Boolean array.
+     * @param size Boolean array size.
+     */
 	void setBooleans(bool * boolArr, uint8_t size);
+
+    /**
+     * Get the strings array.
+     *
+     * @return Strings.
+     */
 	String * getStrings();
+
+    /**
+     * Get the number of strings in the string array.
+     *
+     * @return String array size.
+     */
     uint8_t getStringsCount();
+
+    /**
+     * Check whether this packet has any strings.
+     *
+     * @return True if there are any strings, false otherwise.
+     */
 	bool hasStrings();
+
+    /**
+     * Set the strings array.
+     *
+     * @param intArr String array.
+     * @param size String array size.
+     */
 	void setStrings(String * strArr, uint8_t size);
+
+    /**
+     * Get the number of different type of data arrays included in this packet.
+     *
+     * @return Data array type count.
+     */
     uint8_t getArrayCount();
 };
 
