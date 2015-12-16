84664138ad1d4a2c72fedb74ebcf7d1a2a278e4e
timvisee <timvisee@gmail.com>
2015-12-16 12:45:29 +0100

    Created ButtonManager source file, to fix the static field initialization

---
+++ ButtonManager.cpp
@@ -0,0 +1,11 @@
+/**
+ * NumberGuessGame.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#include "ButtonManager.h"
+
+Button ButtonManager::button = Button(0);
