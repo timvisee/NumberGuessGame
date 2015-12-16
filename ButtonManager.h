5138475b2a1071aa85c6e8d316e5eadda54c13ec
Tim Visée <timvisee@gmail.com>
2015-12-16 01:14:53 +0100

    Created ButtonManager class

---
+++ ButtonManager.h
@@ -0,0 +1,31 @@
+/**
+ * NumberGuessGame.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#ifndef NUMBERGUESSGAME_LEDMANAGER_H
+#define NUMBERGUESSGAME_LEDMANAGER_H
+
+#include <Arduino.h>
+#include "Globals.h"
+#include "Button.h"
+
+/**
+ * Button manager class.
+ */
+class ButtonManager {
+public:
+
+    /**
+     * Main button instance.
+     */
+    static Button button;
+};
+
+// Initialize static field
+Button ButtonManager::button = NULL;
+
+#endif //NUMBERGUESSGAME_LEDMANAGER_H
