55b4d5e9e4aeed318141f9928f93f26b628b48b0
Tim Visée <timvisee@gmail.com>
2015-12-16 18:50:09 +0100

    Temporarily disabled memory reporter

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -19,7 +19,7 @@ void MemoryManager::reportMemory() {
     Log::init();
 
     // Report the number of available memory
-    Log::info("M> " + String(memFree) + " b free");
+    //Log::info("M> " + String(memFree) + " b free");
 }
 
 void MemoryManager::update() {
