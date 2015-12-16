0cf990b667b89fef8f93328cd1ab764ea67e36ac
timvisee <timvisee@gmail.com>
2015-12-16 13:26:11 +0100

    Created method sources of connection manager class

--- ConnectionManager.cpp
+++ ConnectionManager.cpp
@@ -11,3 +11,27 @@
 bool ConnectionManager::multiplayer = true;
 bool ConnectionManager::master = true;
 bool ConnectionManager::connected = false;
+
+bool ConnectionManager::isMultiplayer() {
+    return ConnectionManager::multiplayer;
+}
+
+void ConnectionManager::setMultipayer(bool multiplayer) {
+    ConnectionManager::multiplayer = multiplayer;
+}
+
+bool ConnectionManager::isMaster() {
+    return ConnectionManager::master;
+}
+
+void ConnectionManager::setMaster(bool master) {
+    ConnectionManager::master = master;
+}
+
+bool ConnectionManager::isConnected() {
+    return ConnectionManager::connected;
+}
+
+void ConnectionManager::setConnected(bool connected) {
+    ConnectionManager::connected = connected;
+}
